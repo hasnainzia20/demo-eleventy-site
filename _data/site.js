b4b624bb0a4f2475b2878c1225ea54c288211932
Hasnain Zia <hasnainzia438@gmail.com>
2025-08-28 22:30:51 +0500

    added baseUrl

--- _data/site.js
+++
@@ -1,6 +0,0 @@
-export default {
-  url:
-    process.env.ELEVENTY_ENV === "production"
-      ? "https://tooleble.com"
-      : "http://localhost:8080",
-};
--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,13 @@
 export default {
-  url: process.env.SITE_URL || "https://tooleble.com",
   name: "Tooleble",
+  url: process.env.SITE_URL || "https://tooleble.com",
+  baseUrl: (() => {
+    if (process.env.ELEVENTY_ENV === "development") {
+      return "/"; // local dev
+    }
+    if (process.env.SITE_URL?.includes("github.io")) {
+      return "/demo-eleventy-site/"; // GitHub Pages subpath
+    }
+    return "/"; // production domain
+  })(),
 };
